b50532b34a845a3398383c3a0ebc8fdc526b998f
tino de la Vega <delavegatino02@gmail.com>
2021-08-09 15:30:21 -0500

    Plot 2 code and plot

---
+++ plot2.R
@@ -0,0 +1,19 @@
+library(data.table) #to use the fread() function
+dat <- fread("household_power_consumption.txt") #Read .txt
+dat <- subset(dat, Date == "1/2/2007" | Date == "2/2/2007") #Grab only those 2 days
+
+#Create column and merge together
+FullTimeDate <- strptime(paste(dat$Date, powerdata$Time, sep=" "),
+                         "%d/%m/%Y %H:%M:%S")
+dat <- cbind(dat, FullTimeDate)
+
+## change class of all columns to correct class
+dat$Date <- as.Date(dat$Date, format="%d/%m/%Y")
+dat$Time <- format(dat$Time, format="%H:%M:%S")
+dat$Global_active_power <- as.numeric(dat$Global_active_power)
+
+## plot globalactivepower vs date&time
+png("plot2.png", width=480, height=480)
+with(dat, plot(FullTimeDate, Global_active_power, type="l", xlab="Day", 
+               ylab="Global Active Power (kilowatts)"))
+dev.off()
